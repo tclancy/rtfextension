07e6ac65337c342005422ce0b715421802bce64d
Tom Clancy <tclancy@gmail.com>
2016-04-05 14:08:21 -0400

    Made small progress in communicating with window

---
+++ inpage.js
@@ -0,0 +1,11 @@
+console.log("XXX");
+
+
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    console.log(sender.tab ?
+                "from a content script:" + sender.tab.url :
+                "from the extension");
+    if (request.greeting == "hello")
+      sendResponse({farewell: "goodbye"});
+  });
--- reader.js
+++ reader.js
@@ -1,10 +1,24 @@
 chrome.app.runtime.onLaunched.addListener(function(launchData) {
-    debugger;
     chrome.app.window.create('window.html', {
+        'id': 'rtf',
         'outerBounds': {
         'width': 400,
         'height': 500
         }
+    }, function (rtf_window) {
+        chrome.runtime.sendMessage({greeting: "hello"}, function(response) {
+            if (response) {
+                console.log(response.farewell);
+            } else {
+                console.log("NO CARRIER");
+            }
+        });
     });
-    console.log("called");
+
+
+    // console.log(reader);
+    // chrome.app.window.getAll()[0]
+    // document here refers to the background page and not the window we have open
+    //document.getElementsByTagName("div")[0].textContent = "Bite me";
 });
+
